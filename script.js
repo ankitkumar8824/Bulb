5cd37df703f833e5723a5060a7df51266e9dd30f
ankitkumar8824 <ankitbalania2@gmail.com>
2025-02-09 19:17:03 +0530

    first

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("img").src="of.png";
+
+
+function btnOn(){
+    document.getElementById("img").src="on.png";
+    document.getElementById("on").style.background="yellow"
+    document.getElementById("on").style.color="black"
+    document.getElementById("off").style.background="white"
+    document.getElementById("off").style.color="black"
+}
+
+function btnOff(){
+    document.getElementById("img").src="of.png";
+    document.getElementById("off").style.background="yellow"
+    document.getElementById("off").style.color="black"
+        document.getElementById("on").style.background="white"
+    document.getElementById("on").style.color="black"
+
+}
